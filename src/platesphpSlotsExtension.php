6f6ba521bf8fbd6ecd47557c3a31592158751f8c
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2022-03-12 22:58:58 +0100

    Better slots api - slot into slot

--- src/platesphpSlotsExtension.php
+++ src/platesphpSlotsExtension.php
@@ -2,7 +2,11 @@
         $templates->loadExtension(new class() implements League\Plates\Extension\ExtensionInterface {
             protected $engine;
             public $template; // must be public
-        
+
+            private $openSlots = [];
+            private $slots = [];
+            private $component = null;
+
             public function register($engine) {
                 $this->engine = $engine;
                 
@@ -13,52 +17,48 @@
                 //slots
                 $engine->registerFunction('startSlot', [$this, 'startSlot']);
                 $engine->registerFunction('stopSlot', [$this, 'stopSlot']);
-                $engine->registerFunction('slot', [$this, 'useSlot']);
-                $engine->registerFunction('hasSlot', [$this, 'hasSlot']);
             }
         
             public function component($path, $data) {
                 $this->component = [$path, $data];
-                $this->slots = [];
-                $this->defaultSlot = "";
         
                 ob_start();
                 ob_implicit_flush(0);
             }
         
-            public function endcomponent() {        
-                $this->defaultSlot = ob_get_clean();
+            public function endcomponent() {   
+                $defaultSlot = ob_get_clean();
                 [$path, $data] = $this->component;
-                list($path, $data) = $this->component;
-        
-                $result = $this->template->insert($path, $data);
+                $slots = $this->slots;
                 
-                $this->openSlot = "";
-                $this->componentData = [];
-                $this->slots = [];
-                $this->defaultSlot = "";
+                $slotFunction = function($slotName = null) use($defaultSlot, $slots) {
+                    if($slotName === null) return $defaultSlot;
 
+                    if(array_key_exists($slotName, $slots)) {
+                        return $slots[$slotName];
+                    }
+
+                    return null;
+                };
+
+                $result = $this->template->insert($path, array_merge($data, [
+                    "slot" => $slotFunction
+                ]));
+
+                $this->slots = [];
                 echo $result;
             }
 
             public function startSlot($slotName) {
-                $this->openSlot = $slotName;
+                array_push($this->openSlots, $slotName);
                 ob_start();
                 ob_implicit_flush(0);
             }
 
             public function stopSlot() {
                 $currentSlot = ob_get_clean();
-                $this->slots[$this->openSlot] = $currentSlot;
-                $this->openSlot = "";
-            }
-        
-            public function useSlot($slotName = null) {
-                return $slotName === null ? $this->defaultSlot : $this->slots[$slotName];
+                $slotName = array_pop($this->openSlots);
+                $this->slots[$slotName] = $currentSlot;
             }
 
-            public function hasSlot($slotName = null) {
-                if($slotName === null) return $this->defaultSlot !== "";
-                return array_key_exists($slotName, $this->slots);
-            }
         });
